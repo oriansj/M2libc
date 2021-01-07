8f9925635e903d88cf818bb9175dcbba8b60d4cc
Jeremiah Orians <jeremiah@pdp10.guru>
2021-01-06 23:12:17 -0500

    The bootstrappable library

---
+++ bootstrappable.h
@@ -0,0 +1,143 @@
+/* Copyright (C) 2016 Jeremiah Orians
+ * This file is part of M2-Planet.
+ *
+ * M2-Planet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * M2-Planet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define TRUE 1
+#define FALSE 0
+
+
+void require(int bool, char* error)
+{
+	if(!bool)
+	{
+		fputs(error, stderr);
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+int match(char* a, char* b)
+{
+	int i = -1;
+	do
+	{
+		i = i + 1;
+		if(a[i] != b[i])
+		{
+			return FALSE;
+		}
+	} while((0 != a[i]) && (0 !=b[i]));
+	return TRUE;
+}
+
+
+int in_set(int c, char* s)
+{
+	while(0 != s[0])
+	{
+		if(c == s[0]) return TRUE;
+		s = s + 1;
+	}
+	return FALSE;
+}
+
+
+int index_number(char* s, char c)
+{
+	int i = 0;
+	while(s[i] != c)
+	{
+		i = i + 1;
+		if(0 == s[i]) return -1;
+	}
+	return i;
+}
+
+
+int toupper(int c)
+{
+	if(in_set(c, "abcdefghijklmnopqrstuvwxyz")) return (c & 0xDF);
+	return c;
+}
+
+
+int __set_reader(char* set, int mult, char* input)
+{
+	int n = 0;
+	int i = 0;
+	int hold;
+	int negative_p = FALSE;
+
+	if(input[0] == '-')
+	{
+		negative_p = TRUE;
+		i = i + 1;
+	}
+
+	while(in_set(input[i], set))
+	{
+		n = n * mult;
+		hold = index_number(set, toupper(input[i]));
+
+		/* Input managed to change between in_set and index_number */
+		if(-1 == hold) return 0;
+		n = n + hold;
+		i = i + 1;
+	}
+
+	/* loop exited before NULL and thus invalid input */
+	if(0 != input[i]) return 0;
+
+	if(negative_p)
+	{
+		n = 0 - n;
+	}
+
+	return n;
+}
+
+int strtoint(char *a)
+{
+	/* If NULL string */
+	if(0 == a[0])
+	{
+		return 0;
+	}
+	/* Deal with binary*/
+	else if ('0' == a[0] && 'b' == a[1])
+	{
+		return __set_reader("01", 2, a+2);
+	}
+	/* Deal with hex */
+	else if ('0' == a[0] &&  'x' == a[1])
+	{
+		return __set_reader("0123456789ABCDEFabcdef", 16, a+2);
+	}
+	/* Deal with octal */
+	else if('0' == a[0])
+	{
+		return __set_reader("01234567", 8, a+1);
+	}
+	/* Deal with decimal */
+	else
+	{
+		return __set_reader("0123456789", 10, a);
+	}
+}
