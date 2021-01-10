655be424cc71f24d33f2b87d71e50ac7031fd564
Jeremiah Orians <jeremiah@pdp10.guru>
2021-01-10 11:48:44 -0500

    Prototype armv7l libc

---
+++ armv7l/Linux/unistd.h
@@ -0,0 +1,173 @@
+/* Copyright (C) 2020 Jeremiah Orians
+ * This file is part of M2-Planet.
+ *
+ * M2-Planet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * M2-Planet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define NULL 0
+
+int access(char* pathname, int mode)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!33 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+int chdir(char* path)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!12 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+int fchdir(int fd)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!133 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+void _exit(int value)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!1 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+
+int fork()
+{
+	asm("!2 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+
+int waitpid (int pid, int* status_ptr, int options)
+{
+	asm("!114 R7 LOADI8_ALWAYS"
+	    "!12 R2 SUB R12 ARITH_ALWAYS"
+	    "!0 R2 LOAD32 R2 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+
+int execve(char* file_name, char** argv, char** envp)
+{
+	asm("!11 R7 LOADI8_ALWAYS"
+	    "!12 R2 SUB R12 ARITH_ALWAYS"
+	    "!0 R2 LOAD32 R2 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+int read(int fd, char* buf, unsigned count)
+{
+	asm("!3 R7 LOADI8_ALWAYS"
+	    "!12 R2 SUB R12 ARITH_ALWAYS"
+	    "!0 R2 LOAD32 R2 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+int write(int fd, char* buf, unsigned count)
+{
+	asm("!4 R7 LOADI8_ALWAYS"
+	    "!12 R2 SUB R12 ARITH_ALWAYS"
+	    "!0 R2 LOAD32 R2 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+int lseek(int fd, int offset, int whence)
+{
+	asm("!19 R7 LOADI8_ALWAYS"
+	    "!12 R2 SUB R12 ARITH_ALWAYS"
+	    "!0 R2 LOAD32 R2 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+int close(int fd)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!6 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+int _getcwd(char* buf, int size)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!183 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+int brk(void *addr)
+{
+	asm("!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "!45 R7 LOADI8_ALWAYS"
+	    "SYSCALL_ALWAYS");
+}
+
+int chmod(char *pathname, int mode)
+{
+	asm("!15 R7 LOADI8_ALWAYS"
+	    "!8 R1 SUB R12 ARITH_ALWAYS"
+	    "!0 R1 LOAD32 R1 MEMORY"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
+
+struct utsname
+{
+	char sysname[65];    /* Operating system name (e.g., "Linux") */
+	char nodename[65];   /* Name within "some implementation-defined network" */
+	char release[65];    /* Operating system release (e.g., "2.6.28") */
+	char version[65];    /* Operating system version */
+	char machine[65];    /* Hardware identifier */
+};
+
+int uname(struct utsname* unameData)
+{
+	asm("!122 R7 LOADI8_ALWAYS"
+	    "!4 R0 SUB R12 ARITH_ALWAYS"
+	    "!0 R0 LOAD32 R0 MEMORY"
+	    "SYSCALL_ALWAYS");
+}
