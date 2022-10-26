05557684eae234634a282179225b419031b31f05
Jacob McSwain <jacob@mcswain.dev>
2022-10-26 16:58:52 -0500

    aarch64: syscall: unlink -> unlinkat

--- aarch64/linux/unistd.c
+++ aarch64/linux/unistd.c
@@ -138,8 +138,10 @@ int close(int fd)
 
 int unlink (char* filename)
 {
-	asm("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
-	    "SET_X8_TO_SYS_UNLINK"
+	asm("SET_X1_FROM_X0"
+	    "SET_X2_TO_0"
+	    "SET_X0_TO_FCNTL_H_AT_FDCWD"
+	    "SET_X8_TO_SYS_UNLINKAT"
 	    "SYSCALL");
 }
 
