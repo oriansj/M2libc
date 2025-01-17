84aa4bbcb149d444e53acbfff2d6ef65d8457d06
gtker <git@gtker.com>
2025-01-17 23:16:59 +0100

    Change bool define to _Bool
    This type will be added to the compiler.

--- stdbool.h
+++ stdbool.h
@@ -20,8 +20,7 @@
 
 #define __bool_true_false_are_defined 1
 
-/* Not standard compliant but we don't have the _Bool type */
-#define bool char
+#define bool _Bool
 
 #define true 1
 #define false 0
