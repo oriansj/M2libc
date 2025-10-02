f452a61d7eb96e9e12cd62c8967bc71efa2fc2d6
Gtker <github@gtker.com>
2025-10-02 14:58:56 +0200

    aarch64: Add naive fread
    Somehow I missed this in https://github.com/oriansj/M2libc/pull/94.

--- aarch64/linux/bootstrap.c
+++ aarch64/linux/bootstrap.c
@@ -57,6 +57,17 @@ int fgetc(FILE* f)
 	    "SET_X0_TO_MINUS_1");
 }
 
+unsigned fread(char* buffer, unsigned size, unsigned count, FILE* f) {
+	count = size * count;
+
+	unsigned i = 0;
+	for(; i < count; i = i + 1) {
+		buffer[i] = fgetc(f);
+	}
+
+	return i;
+}
+
 void fputc(char s, FILE* f)
 {
 	asm("SET_X0_FROM_BP" "SUB_X0_8"
