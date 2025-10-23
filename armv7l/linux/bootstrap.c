5bc82bf325327afb58c4fd00292e099502cece9a
Gtker <github@gtker.com>
2025-10-23 13:43:54 +0200

    Refactor arch specific bootstrap.c to only contain syscall wrappers
    This means that users must now include the arch specific .c file first
    and then the general bootstrap.c.
    
    This should make it way easier to maintain the existing architectures
    and potentially add new ones later.
    
    knight-native can not be treated like this since it doesn't have
    read/write syscalls, only fputc/fgetc.

--- armv7l/linux/bootstrap.c
+++ armv7l/linux/bootstrap.c
@@ -15,34 +15,6 @@
  * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-enum
-{
-	stdin = 0,
-	stdout = 1,
-	stderr = 2,
-};
-
-enum
-{
-	EOF = 0xFFFFFFFF,
-	NULL = 0,
-};
-
-enum
-{
-	EXIT_FAILURE = 1,
-	EXIT_SUCCESS = 0,
-};
-
-enum
-{
-	TRUE = 1,
-	FALSE = 0,
-};
-
-
-void* malloc(int size);
-
 unsigned read(FILE* f, char* buffer, unsigned count) {
 	asm("!4 R0 SUB R12 ARITH_ALWAYS"
 	    "!0 R0 LOAD32 R0 MEMORY"
@@ -54,26 +26,6 @@ unsigned read(FILE* f, char* buffer, unsigned count) {
 	    "SYSCALL_ALWAYS");
 }
 
-
-char* __fputc_buffer;
-int fgetc(FILE* f)
-{
-	/* We don't have operator & */
-	if(__fputc_buffer == NULL) {
-		__fputc_buffer = malloc(1);
-	}
-
-	if(read(f, __fputc_buffer, 1) <= 0) {
-		return EOF;
-	}
-
-	return __fputc_buffer[0];
-}
-
-unsigned fread(char* buffer, unsigned size, unsigned count, FILE* f) {
-	return read(f, buffer, size * count);
-}
-
 unsigned write(FILE* f, char* buffer, unsigned count) {
 	asm("!4 R0 SUB R12 ARITH_ALWAYS"
 	    "!0 R0 LOAD32 R0 MEMORY"
@@ -83,25 +35,6 @@ unsigned write(FILE* f, char* buffer, unsigned count) {
 	    "!0 R2 LOAD32 R2 MEMORY"
 	    "!4 R7 LOADI8_ALWAYS"
 	    "SYSCALL_ALWAYS");
-}
-
-void fputc(char s, FILE* f)
-{
-	/* We don't have operator & */
-	if(__fputc_buffer == NULL) {
-		__fputc_buffer = malloc(1);
-	}
-	__fputc_buffer[0] = s;
-
-	write(f, __fputc_buffer, 1);
-}
-
-unsigned fwrite(char* buffer, unsigned size, unsigned count, FILE* f) {
-	if(size == 0 || count == 0) {
-		return 0;
-	}
-
-	return write(f, buffer, size * count);
 }
 
 FILE* open(char* name, int flag, int mode)
@@ -116,37 +49,11 @@ FILE* open(char* name, int flag, int mode)
 	    "SYSCALL_ALWAYS");
 }
 
-FILE* fopen(char* filename, char* mode)
-{
-	FILE* f;
-	if('w' == mode[0])
-	{ /* 577 is O_WRONLY|O_CREAT|O_TRUNC, 384 is 600 in octal */
-		f = open(filename, 577 , 384);
-	}
-	else
-	{ /* Everything else is a read */
-		f = open(filename, 0, 0);
-	}
-
-	/* Negative numbers are error codes */
-	if(0 > f)
-	{
-		return 0;
-	}
-	return f;
-}
-
 int close(int fd)
 {
 	asm("!4 R0 SUB R12 ARITH_ALWAYS"
 	    "!6 R7 LOADI8_ALWAYS"
 	    "SYSCALL_ALWAYS");
-}
-
-int fclose(FILE* stream)
-{
-	int error = close(stream);
-	return error;
 }
 
 int brk(void *addr)
@@ -155,63 +62,6 @@ int brk(void *addr)
 	    "!0 R0 LOAD32 R0 MEMORY"
 	    "!45 R7 LOADI8_ALWAYS"
 	    "SYSCALL_ALWAYS");
-}
-
-long _malloc_ptr;
-long _brk_ptr;
-
-void* malloc(int size)
-{
-	if(NULL == _brk_ptr)
-	{
-		_brk_ptr = brk(0);
-		_malloc_ptr = _brk_ptr;
-	}
-
-	if(_brk_ptr < _malloc_ptr + size)
-	{
-		_brk_ptr = brk(_malloc_ptr + size);
-		if(-1 == _brk_ptr) return 0;
-	}
-
-	long old_malloc = _malloc_ptr;
-	_malloc_ptr = _malloc_ptr + size;
-	return old_malloc;
-}
-
-int strlen(char* str )
-{
-	int i = 0;
-	while(0 != str[i]) i = i + 1;
-	return i;
-}
-
-void fputs(char* s, FILE* f)
-{
-	write(f, s, strlen(s));
-}
-
-void* memset(void* ptr, int value, int num)
-{
-	char* s;
-	for(s = ptr; 0 < num; num = num - 1)
-	{
-		s[0] = value;
-		s = s + 1;
-	}
-}
-
-void* calloc(int count, int size)
-{
-	void* ret = malloc(count * size);
-	if(NULL == ret) return NULL;
-	memset(ret, 0, (count * size));
-	return ret;
-}
-
-void free(void* l)
-{
-	return;
 }
 
 void exit(int value)
--- x86/linux/bootstrap.c
+++ x86/linux/bootstrap.c
@@ -15,33 +15,6 @@
  * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-enum
-{
-	stdin = 0,
-	stdout = 1,
-	stderr = 2,
-};
-
-enum
-{
-	EOF = 0xFFFFFFFF,
-	NULL = 0,
-};
-
-enum
-{
-	EXIT_FAILURE = 1,
-	EXIT_SUCCESS = 0,
-};
-
-enum
-{
-	TRUE = 1,
-	FALSE = 0,
-};
-
-void* malloc(int size);
-
 unsigned read(FILE* f, char* buffer, unsigned count) {
 	asm("mov_eax, %3"
 		"lea_ecx,[esp+DWORD] %8"
@@ -53,26 +26,6 @@ unsigned read(FILE* f, char* buffer, unsigned count) {
 		"int !0x80");
 }
 
-char* __fputc_buffer;
-int fgetc(FILE* f)
-{
-	/* We don't have operator & */
-	if(__fputc_buffer == NULL) {
-		__fputc_buffer = malloc(1);
-	}
-
-	if(read(f, __fputc_buffer, 1) <= 0) {
-		return EOF;
-	}
-
-	return __fputc_buffer[0];
-}
-
-unsigned fread(char* buffer, unsigned size, unsigned count, FILE* f) {
-	return read(f, buffer, size * count);
-}
-
-
 unsigned write(FILE* f, char* buffer, unsigned count) {
 	asm("mov_eax, %4"
 		"lea_ecx,[esp+DWORD] %8"
@@ -82,37 +35,6 @@ unsigned write(FILE* f, char* buffer, unsigned count) {
 		"lea_ebx,[esp+DWORD] %12"
 		"mov_ebx,[ebx]"
 		"int !0x80");
-}
-
-void fputc(char s, FILE* f)
-{
-	/* We don't have operator & */
-	if(__fputc_buffer == NULL) {
-		__fputc_buffer = malloc(1);
-	}
-	__fputc_buffer[0] = s;
-
-	write(f, __fputc_buffer, 1);
-}
-
-unsigned fwrite(char* buffer, unsigned size, unsigned count, FILE* f) {
-	if(size == 0 || count == 0) {
-		return 0;
-	}
-
-	return write(f, buffer, size * count);
-}
-
-int strlen(char* str )
-{
-	int i = 0;
-	while(0 != str[i]) i = i + 1;
-	return i;
-}
-
-void fputs(char* s, FILE* f)
-{
-	write(f, s, strlen(s));
 }
 
 FILE* open(char* name, int flag, int mode)
@@ -127,38 +49,12 @@ FILE* open(char* name, int flag, int mode)
 		"int !0x80");
 }
 
-FILE* fopen(char* filename, char* mode)
-{
-	FILE* f;
-	if('w' == mode[0])
-	{ /* 577 is O_WRONLY|O_CREAT|O_TRUNC, 384 is 600 in octal */
-		f = open(filename, 577 , 384);
-	}
-	else
-	{ /* Everything else is a read */
-		f = open(filename, 0, 0);
-	}
-
-	/* Negative numbers are error codes */
-	if(0 > f)
-	{
-		return 0;
-	}
-	return f;
-}
-
 int close(int fd)
 {
 	asm("lea_ebx,[esp+DWORD] %4"
 	    "mov_ebx,[ebx]"
 	    "mov_eax, %6"
 	    "int !0x80");
-}
-
-int fclose(FILE* stream)
-{
-	int error = close(stream);
-	return error;
 }
 
 int brk(void *addr)
@@ -168,51 +64,6 @@ int brk(void *addr)
 	    "mov_eax, %45"
 	    "pop_ebx"
 	    "int !0x80");
-}
-
-long _malloc_ptr;
-long _brk_ptr;
-
-void* malloc(int size)
-{
-	if(NULL == _brk_ptr)
-	{
-		_brk_ptr = brk(0);
-		_malloc_ptr = _brk_ptr;
-	}
-
-	if(_brk_ptr < _malloc_ptr + size)
-	{
-		_brk_ptr = brk(_malloc_ptr + size);
-		if(-1 == _brk_ptr) return 0;
-	}
-
-	long old_malloc = _malloc_ptr;
-	_malloc_ptr = _malloc_ptr + size;
-	return old_malloc;
-}
-
-void* memset(void* ptr, int value, int num)
-{
-	char* s;
-	for(s = ptr; 0 < num; num = num - 1)
-	{
-		s[0] = value;
-		s = s + 1;
-	}
-}
-
-void* calloc(int count, int size)
-{
-	void* ret = malloc(count * size);
-	if(NULL == ret) return NULL;
-	memset(ret, 0, (count * size));
-	return ret;
-}
-
-void free(void* l)
-{
-	return;
 }
 
 void exit(int value)
